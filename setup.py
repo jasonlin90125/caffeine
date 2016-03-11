27840957488a2a99f00a00b8b83e3c9096a3152a
marta-sd <martasd@ibb.waw.pl>
2016-03-11 08:09:55 +0100

    version 1.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='DeCAF',
-    version='1.0.2',
+    version='1.0.3',
     description='Discrimination, Comparison, Alignment tool for small molecules',
     author='Marta M. Stepniewska',
     author_email='martasd@ibb.waw.pl',
