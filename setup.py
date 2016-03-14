f9d1bfa43646e777577aa1f3a20596cecb47d6d3
marta-sd <martasd@ibb.waw.pl>
2016-03-14 07:38:25 +0100

    license in setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author='Marta M. Stepniewska',
     author_email='martasd@ibb.waw.pl',
     url='https://bitbucket.org/marta-sd/decaf',
+    license = 'BSD',
     packages=['decaf',
               'decaf.toolkits'],
     install_requires=['numpy>=1.6.2', 'scipy>=0.10', 'matplotlib>=1.3.1']
