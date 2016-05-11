jasonlin90125/caffeine

commit a010bf4ff6ffbfa1b38de85b3545283ef382bd84

checking all coarse-grained alignments with the highest score when looking for the best fine-grained alignment

decaf/utils.py
@@ -642,10 +642,10 @@ def map_pharmacophores(p1, p2, dist_tol=0.0, coarse_grained=True):
     dist2 = distances(p2)
     dist2[p2.edges > 0] = p2.edges[p2.edges > 0]
 
-    idx1 = []
-    idx2 = []
-    n1 = []
-    n2 = []
+    idx1 = [[]]
+    idx2 = [[]]
+    n1 = [[]]
+    n2 = [[]]
     score = 0.0
     cost = 0.0
 
@@ -661,34 +661,61 @@ def map_pharmacophores(p1, p2, dist_tol=0.0, coarse_grained=True):
         s = np.sum(scores[clique])
         c = np.sum(costs[clique, :][:, clique]) / 2
 
-        if (s - c > scorecost) or (s - c == scorecost and s > score):
-            score = s
-            cost = c
-            scorecost = s - c
+        if (s - c >= scorecost):
+            if (s - c > scorecost) or (s - c == scorecost and s > score):
+                # replace current solutions with a better one
+                score = s
+                cost = c
+                scorecost = s - c
+    
+                idx1 = [[]]
+                idx2 = [[]]
+                n1 = [[]]
+                n2 = [[]]
+
+            else:
+                # remember another solution with same score
+                idx1.append([])
+                idx2.append([])
+                n1.append([])
+                n2.append([])
 
             rings = [i for i in clique if i in ring_pairs]
-            idx1 = []
-            idx2 = []
-            n1 = []
-            n2 = []
 
             for pair in clique:
                 if pair in rings:
-                    n1.append(np.array(nodes[pair]["members"][0]))
-                    n2.append(np.array(nodes[pair]["members"][1]))
+                    n1[-1].append(np.array(nodes[pair]["members"][0]))
+                    n2[-1].append(np.array(nodes[pair]["members"][1]))
                 else:
-                    idx1.append(nodes[pair]["n1"])
-                    idx2.append(nodes[pair]["n2"])
+                    idx1[-1].append(nodes[pair]["n1"])
+                    idx2[-1].append(nodes[pair]["n2"])
 
     if not coarse_grained:
-        score, cost, [idx1, idx2] = __align_rings(p1, p2, n1, n2, idx1, idx2,
-                                                  mapping, dist1, dist2,
-                                                  dist_tol)
-    else:
-        idx1 += n1
-        idx2 += n2
+        score = 0.0
+        cost = 0.0
+        scorecost = float("-inf")
+        aln1 = []
+        aln2 = []
 
-    return score, cost, [idx1, idx2]
+        for i in xrange(len(idx1)):
+            s, c, [tmp1, tmp2] = __align_rings(p1, p2, n1[i], n2[i],
+                                               idx1[i], idx2[i],
+                                               mapping, dist1, dist2,
+                                               dist_tol)
+            if (s - c > scorecost) or (s - c == scorecost and s > score):
+                score = s
+                cost = c
+                scorecost = s - c
+                aln1 = tmp1[:]
+                aln2 = tmp2[:]
+
+    else:
+        aln1 = idx1[0]
+        aln2 = idx2[0]
+        aln1 += n1
+        aln2 += n2
+
+    return score, cost, [aln1, aln2]
 
 
 def similarity(p1, p2, dist_tol=0.0, coarse_grained=True):

commit c220f75e4fd450f72c4a61e74fe670b5c76759e0

fixed cost calculations and duplicated rings in coarse-grained alignment  (bug introduced in a010bf4)

decaf/utils.py
@@ -645,7 +645,13 @@ def __add_neighbours(p1, p2, n1, n2, idx1, idx2, mapping=None,
                         pairs.append((neigh1, neigh2))
 
     score = np.sum(mapping[idx1, idx2])
-    cost = np.sum(np.abs(p1.edges[idx1][:, idx1] - p2.edges[idx2][:, idx2])) / 2.0
+
+    dist_diff = np.abs(p1.edges[idx1][:, idx1] - p2.edges[idx2][:, idx2])
+    connected = np.where(p1.edges[idx1][:, idx1] * p2.edges[idx2][:, idx2])
+    if len(connected[0]) > 0:
+        assert np.max(dist_diff[connected]) <= dist_tol, "cost too high"
+    cost = np.sum(dist_diff[connected]) / 2.0
+
     aln = [idx1[:], idx2[:]]
 
     for i, pair in enumerate(pairs):
@@ -793,8 +799,8 @@ def map_pharmacophores(p1, p2, dist_tol=0.0, coarse_grained=True):
     else:
         aln1 = idx1[0]
         aln2 = idx2[0]
-        aln1 += n1
-        aln2 += n2
+        aln1 += n1[0]
+        aln2 += n2[0]
 
     return score, cost, [aln1, aln2]
 

setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='DeCAF',
-    version='1.1.0',
+    version='1.1.1',
     description='Discrimination, Comparison, Alignment tool for small molecules',
     author='Marta M. Stepniewska',
     author_email='martasd@ibb.waw.pl',