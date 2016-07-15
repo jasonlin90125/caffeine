d40a690e29ccbce3953e70367f954e9dacaa47b9
marta-sd <martasd@ibb.waw.pl>
2016-07-15 11:08:15 +0200

    setup and test reorganisation

--- setup.py
+++ setup.py
@@ -6,11 +6,17 @@ setup(
     name='DeCAF',
     version='2.0.0',
     description='Discrimination, Comparison, Alignment tool for small molecules',
+    keywords=['cheminformatics', 'small molecules', 'pharmacophore', 'graph',
+              'alignment'],
     author='Marta M. Stepniewska',
     author_email='martasd@ibb.waw.pl',
     url='https://bitbucket.org/marta-sd/decaf',
     license='BSD',
     packages=['decaf',
-              'decaf.toolkits'],
-    install_requires=['numpy>=1.6.2', 'scipy>=0.10', 'matplotlib>=1.3.1']
+              'decaf.toolkits',
+              'tests'],
+    setup_requires=['numpy>=1.6.2'],
+    install_requires=['numpy>=1.6.2', 'scipy>=0.10', 'matplotlib>=1.3.1'],
+    test_suite='tests',
+    package_data={'tests': ['tests/*.phar']}
 )
